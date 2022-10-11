6f879cf4552eca7ef617e642f023bc9e529b8553
Syuan <a733181@gmail.com>
2022-10-11 13:11:42 +0800

    修改計算機

--- src/app.js
+++ src/app.js
@@ -1,116 +1,58 @@
 'use strict';
-
-const clearEl = document.querySelector('.clear');
-const showCountEl = document.querySelector('.show-county');
-const numbersAllEl = document.querySelectorAll('.numbers');
-const arithmeticAllEl = document.querySelectorAll('.arithmetic');
+const sumEl = document.querySelector('.sum');
+const btnsEl = document.querySelectorAll('.county-box li');
 const showAllEl = document.querySelector('.show-all');
 
-let countArr = [];
+let isCount = false;
 
-numbersAllEl.forEach((numbersEl) => {
-  numbersEl.addEventListener;
-  numbersEl.onclick = (e) => {
-    const value = e.target.innerText;
-    if (value.length > 1) {
+btnsEl.forEach((btnEl) => {
+  btnEl.addEventListener('click', () => {
+    calculatorHandler(btnEl.innerText);
+  });
+});
+
+document.addEventListener('keydown', (e) => {
+  if (
+    !isNaN(Number(e.key)) ||
+    ['+', '-', '*', '/', '.', 'Enter', 'C', 'c'].includes(e.key)
+  ) {
+    calculatorHandler(e.key);
+  }
+});
+
+function calculatorHandler(value) {
+  const text = value;
+  const isString = isNaN(Number(text));
+  const sumText = sumEl.innerText;
+  const lastIsString = isNaN(Number(sumText[sumText.length - 1]));
+
+  if ((text === '=' || text === 'Enter') && isCount) {
+    if (lastIsString) {
+      return;
+    }
+    showAllEl.innerText = sumText;
+    sumEl.innerText = eval(sumText);
+    isCount = false;
+  } else if (text === 'C' || text === 'c') {
+    sumEl.innerText = '0';
+    showAllEl.innerText = '';
+    isCount = false;
+  } else if (text !== 'Enter') {
+    if ((sumText === '-' || sumText === '.') && isString) {
       return;
     }
 
-    if (value === 'C') {
-      renderPlaceholderHander();
-      renderHander();
-    } else {
-      renderHander(value);
-    }
-  };
-});
-
-arithmeticAllEl.forEach((arithmeticEl) => {
-  arithmeticEl.onclick = (e) => {
-    const value = e.target.innerText;
-    if (value > 1) {
+    if (lastIsString && isString) {
       return;
     }
-    arithmeticHander(value);
-  };
-});
 
-document.body.onkeydown = (e) => {
-  if (countArr) {
-    showCountEl.setAttribute('placeholder', '');
-  }
-
-  if (e.key >= 0 || e.key <= 9 || e.key === '.') {
-    renderHander(e.key);
-  } else if (e.key === 'c' || e.key === 'C') {
-    renderHander();
-  } else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
-    arithmeticHander(e.key);
-  } else if (e.key === 'Enter') {
-    arithmeticHander('=');
-  }
-};
-
-function renderHander(text = '') {
-  let render = text ? showCountEl.value + text : text;
-  if (render === 'Infinity' || isNaN(render)) {
-    render = 'Error';
-  }
-
-  showAllEl.innerText = countArr.join('');
-  showCountEl.value = render;
-}
-
-function arithmeticHander(arithmetic) {
-  countArr.push(+showCountEl.value);
-  countArr.push(arithmetic);
-  renderHander();
-
-  if (arithmetic === '=') {
-    const lastArithmeticIndex = countArr.length - 3;
-    const lastIsNaN = isNaN(Number(countArr[lastArithmeticIndex]));
-
-    if (lastIsNaN && countArr[lastArithmeticIndex + 1] === 0) {
-      countArr.splice(lastArithmeticIndex, 2);
-      renderHander();
-    }
-    countHander();
-  }
-}
-
-function countHander() {
-  let sum = countArr[0];
-
-  for (let i = 0; i <= countArr.length - 1; i++) {
-    if (i % 2 !== 0) {
-      if (countArr[i] === '=') {
-        break;
+    if (sumText !== '0') {
+      sumEl.innerText += text;
+      if (!isString) {
+        isCount = true;
       }
-      switch (countArr[i]) {
-        case '+': {
-          sum += countArr[++i];
-          break;
-        }
-        case '-': {
-          sum -= countArr[++i];
-          break;
-        }
-        case '*': {
-          sum *= countArr[++i];
-          break;
-        }
-        case '/': {
-          sum /= countArr[++i];
-          break;
-        }
-      }
+    } else if (!['+', '*', '/'].includes(text)) {
+      sumEl.innerText = text;
     }
-  }
-  renderPlaceholderHander(sum);
-}
-function renderPlaceholderHander(text = '') {
-  showCountEl.setAttribute('placeholder', text);
-  if (text !== '') {
-    countArr = [];
   }
 }
